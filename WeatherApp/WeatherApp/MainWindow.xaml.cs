00f6df0206a528e238604aa78706d1d488fb1404
krupowies <krupowieswojciech@gmail.com>
2019-03-22 17:56:40 +0100

    Temperature Update
    gotta improve filepath

--- WeatherApp/WeatherApp/MainWindow.xaml.cs
+++ WeatherApp/WeatherApp/MainWindow.xaml.cs
@@ -12,20 +12,61 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net;
+using System.IO;
 
 namespace WeatherApp
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        public static string GetDataFromWeb()
+        {
+            string URL = "http://meteo.gig.eu/";
+            string file = @"C:\Users\krupo\OneDrive\Pulpit\Weather-station-\WeatherApp\WeatherApp\html.txt";
+            WebClient client = new WebClient();
+            Stream data = client.OpenRead(URL);
+            StreamReader reader = new StreamReader(data);
+
+            string htmlCode = reader.ReadToEnd();
+
+            data.Close();
+            reader.Close();
+            File.WriteAllText(file, htmlCode);
+            return TrimHTML(htmlCode, "µg/m³<br/>Temperatura: ", "°C<br/>Wilgotność");
+        }
+
+        public static string TrimHTML(string strSource, string strStart, string strEnd)
+        {
+            int Start, End;
+            if (strSource.Contains(strStart) && strSource.Contains(strEnd))
+            {
+                Start = strSource.IndexOf(strStart, 0) + strStart.Length;
+                End = strSource.IndexOf(strEnd, Start);
+                return strSource.Substring(Start, End - Start);
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        public void FillTextBox()
+        {
+            string trimmedHTML = GetDataFromWeb();
+            string file = @"C:\Users\krupo\OneDrive\Pulpit\Weather-station-\WeatherApp\WeatherApp\debug.txt";
+            File.WriteAllText(file, trimmedHTML);
+            TemperatureBox.Text = trimmedHTML.ToString();
+        }
+        
+
         private void Update_Click(object sender, RoutedEventArgs e)
         {
+            FillTextBox();
         }
     }
 }
