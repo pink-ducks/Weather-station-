10217e28ee15bca36d0209d1fe10c1c717de188a
krupowies <krupowieswojciech@gmail.com>
2019-03-23 11:45:37 +0100

    Temperature update

--- WeatherApp/WeatherApp/MainWindow.xaml.cs
+++ WeatherApp/WeatherApp/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net;
+using System.IO;
 
 namespace WeatherApp
 {
@@ -24,8 +26,58 @@ namespace WeatherApp
         {
             InitializeComponent();
         }
+
+        public static string GetDataFromWeb()
+        {
+            string URL = "http://meteo.gig.eu/";
+            string file = @"C:\Users\krupo\OneDrive\Pulpit\Weather-station-\WeatherApp\WeatherApp\html.txt";
+            WebClient client = new WebClient();
+            Stream data = client.OpenRead(URL);
+            StreamReader reader = new StreamReader(data);
+
+            string htmlCode = reader.ReadToEnd();
+
+            data.Close();
+            reader.Close();
+            File.WriteAllText(file, htmlCode);
+            return TrimHTML(htmlCode, "µg/m³<br/>Temperatura: ", "°C<br/>Wilgotność");
+        }
+
+        public static string TrimHTML(string strSource, string strStart, string strEnd)
+        {
+            int Start, End;
+            if (strSource.Contains(strStart) && strSource.Contains(strEnd))
+            {
+                Start = strSource.IndexOf(strStart, 0) + strStart.Length;
+                End = strSource.IndexOf(strEnd, Start);
+                return strSource.Substring(Start, End - Start);
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+
+        public void FillTextBox()
+        {
+            string trimmedHTML = GetDataFromWeb();
+            string file = @"C:\Users\krupo\OneDrive\Pulpit\Weather-station-\WeatherApp\WeatherApp\debug.txt";
+            File.WriteAllText(file, trimmedHTML);
+            TemperatureBox.Text = trimmedHTML.ToString();
+            ConditionBox.Text = "Sunny";
+        }
+
+
+
         private void Update_Click(object sender, RoutedEventArgs e)
         {
+            FillTextBox();
+        }
+
+        private void ConditionBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
         }
     }
 }
